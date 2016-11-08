1641355a39c4b25d122d3cfd53d396903b91f48a
martin byrne <marb61a@gmail.com>
2016-11-08 19:47:39 +0000

    added functions file and split html into partials

---
+++ footer.php
@@ -0,0 +1,43 @@
+<div class="clearfix">
+        </div>
+        
+        <footer class="main-footer">
+            <div class="large-3 columns">
+                <h3>About Us</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium efficitur justo ut malesuada.
+                Donec efficitur commodo est, sed maximus nisi pulvinar ac</p>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+                <a href="#"><i class="fa fa-youtube"></i></a>
+            </div> 
+            <div class="large-3 columns">
+                <h3>Categories</h3>   
+                <ul>
+                    <li><a href="#">Some Link</a></li>
+                    <li><a href="#">Some Link</a></li>
+                    <li><a href="#">Some Link</a></li>
+                    <li><a href="#">Some Link</a></li>
+                </ul>
+            </div> 
+            <div class="large-3 columns">
+                <h3>Recent Posts</h3> 
+                <ul>
+                    <li><a href="#">Some Post</a></li>
+                    <li><a href="#">Some Post</a></li>
+                    <li><a href="#">Some Post</a></li>
+                    <li><a href="#">Some Post</a></li>
+                </ul>
+            </div> 
+            <div class="large-3 columns">
+                <h3>Our Location</h3>
+                <iframe></iframe>
+            </div> 
+        </footer>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/what-input.js"></script>
+        <script src="js/vendor/foundation.js"></script>
+        <script src="js/app.js"></script>    
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head class="no-js" lang="en" dir="ltr">>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Clean Template</title>
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/hover.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="top-bar">
+            <div class="row">
+                <div class="large-3 columns social">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a>
+                </div>
+                <div class="large-3 columns">
+                    <span>
+                        <i class="fa fa-phone"></i> Phone: 1 800 555-5555
+                    </span>
+                </div>
+                <div class="large-3 columns">
+                    <span>
+                        <i class="fa fa-envelope"></i> Email: something@something.com
+                    </span>
+                </div>
+                <div class="large-3 columns">
+                    <form>
+                        <input type="text" name="s" placeholder="Search...">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>  
+        </div>
+        
+        <header>
+            <div class="large-4 columns">
+                <a href="index.html"><h3>Clean Theme</h3></a>    
+            </div>
+            <div class="large-8 columns">
+                <ul class="menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                </ul>
+            </div>
+        </header>
